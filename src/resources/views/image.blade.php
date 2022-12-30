e2f513f8c2288d8e5a542469e71bce112b057650
Md abdul karim <karim.cse007@gmail.com>
2022-12-31 05:35:54 +0600

    ...

--- src/resources/views/image.blade.php
+++ src/resources/views/image.blade.php
@@ -1,13 +1,11 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<p>
-    <button class="btn btn-primary btn-lg glyphicon glyphicon-refresh" type="button">
-        <img src="{{ $route }}" id="cap"
-             alt="https://github.com/karim-007/laravel-captcha"
-             style="cursor:pointer;width:{{ $width }}px;height:{{ $height }}px;"
-             title="{{ $title }}"
-             onclick="this.setAttribute('src','{{ $route }}?_='+Math.random());var captcha=document.getElementById('{{ $input_id }}');if(captcha){captcha.focus()}"
-        ><i style="cursor:pointer;width:30px;height:30px;" class="material-icons" onclick="document.getElementById('cap').setAttribute('src','{{ $route }}?_='+Math.random());var captcha=document.getElementById('{{ $input_id }}');if(captcha){captcha.focus()}"
-        >refresh</i>
-    </button>
-</p>
+<div style="margin-bottom: 2px;background: {{config('captcha.background')}}"c>
+    <img src="{{ $route }}" id="cap"
+         alt="https://github.com/karim-007/laravel-captcha"
+         style="cursor:pointer;width:{{ $width }}px;height:{{ $height }}px;"
+         title="{{ $title }}"
+         onclick="this.setAttribute('src','{{ $route }}?_='+Math.random());var captcha=document.getElementById('{{ $input_id }}');if(captcha){captcha.focus()}"
+    ><i style="cursor:pointer;width:30px;height:30px; color: {{config('captcha.colors')}}" class="material-icons" onclick="document.getElementById('cap').setAttribute('src','{{ $route }}?_='+Math.random());var captcha=document.getElementById('{{ $input_id }}');if(captcha){captcha.focus()}"
+    >refresh</i>
+</div>
 
